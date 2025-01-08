ac87f9abdafb78394ef11638d6d92586ca099a5b
ANDRIANAVALONA Maminirina Rolland <0359@production.local>
2025-01-08 07:31:23 +0300

    base

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+# -*- coding : "utf-8" -*-
+from tkinter import *
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class App:
+    print("hello world")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    App()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
